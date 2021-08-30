1a83268f9f7e30cabca1b42b89f895059bf4708c
Adithya Vardhan <imadithyavardhan@gmail.com>
2021-08-30 16:50:11 +0530

    Indicate link copied

--- src/components/CopyLink.js
+++ src/components/CopyLink.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const CopyLink = () => {
+  const [copied, setCopied] = useState(false);
   const history = useHistory();
   return (
-    <div className="copylinkwrap d-flex justify-content-between align-items-center px-3 noselect">
+    <div className="copylinkwrap mt-3 d-flex justify-content-between align-items-center px-3 noselect">
       <div className="copylink">
         <span className="copyheadline mr-2 d-none d-sm-inline">
           Paper Plane Link:
@@ -14,9 +15,13 @@ const CopyLink = () => {
       <div className="d-flex">
         <div
           className="copybtn pointer mr-2"
-          onClick={() =>
-            navigator.clipboard.writeText("https://paper-planeee.web.app/fly")
-          }
+          onClick={() => {
+            navigator.clipboard.writeText("https://paper-planeee.web.app/fly");
+            setCopied(true);
+            setTimeout(() => {
+              setCopied(false);
+            }, 3000);
+          }}
         >
           Copy
         </div>
@@ -26,6 +31,14 @@ const CopyLink = () => {
         >
           Go
         </div>
+      </div>
+      <div
+        className={
+          "position-absolute rounded start-50 px-3 py-1 zindex10 translate-middle-x aftercopy " +
+          (copied ? "copied" : "")
+        }
+      >
+        Copied to clipboard
       </div>
     </div>
   );
